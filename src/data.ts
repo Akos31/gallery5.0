57d780efc2baede08f78f06d35b749e4dd89bdfa
Akos31 <akoskelemen2005@gmail.com>
2024-06-12 21:36:18 +0200

    Pictures uploaded

--- src/data.ts
+++ src/data.ts
@@ -1,52 +1,92 @@
 export const slides = [
   {
-    src: 'https://images.unsplash.com/photo-1682695796954-bad0d0f59ff1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title One',
-    description: 'Slide Description One\n\nthis is new line',
+    src: '/1.jpg',
+    title: 'Ballerini',
+    description: 'Kaposvár',
   },
   {
-    src: 'https://images.unsplash.com/photo-1682686581551-867e0b208bd1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Two',
-    description: 'Slide Description Two',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1682695797221-8164ff1fafc9?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Three',
-    description: 'Slide Description Three',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700744228339-03c725f5ee57?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Four',
-    description: 'Slide Description Four',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700839154423-83ea2246621b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Five',
-    description: 'Slide Description Five',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700770845346-7e95de335eb0?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Six',
-    description: 'Slide Description Six',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700751639138-e6628c42b558?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Seven',
-    description: 'Slide Description Seven',
-  },
-  {
-    src: 'https://plus.unsplash.com/premium_photo-1698442704192-086265cf630e?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Eight',
-    description: 'Slide Description Eight',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700753227268-8832c285591e?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Nine',
-    description: 'Slide Description Nine',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1700769025506-6c3dcb9ec9b7?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    title: 'Slide Title Ten',
-    description: 'Slide Description Ten',
+    src: '/2.jpg',
+    title: 'Zarka',
+    description: 'Kaposvár',
+  },  {
+    src: '/3.jpg',
+    title: 'Toyota Celica',
+    description: 'Fonyód',
+  },  {
+    src: '/4.jpg',
+    title: 'Toyota Celica',
+    description: 'Fonyód',
+  },  {
+    src: '/5.jpg',
+    title: 'Ball',
+    description: 'Kaposvár',
+  },  {
+    src: '/6.jpg',
+    title: 'Lendi',
+    description: 'Kaposvár',
+  },{
+    src: '/7.jpg',
+    title: 'Lendi',
+    description: 'Kaposvár',
+  },{
+    src: '/8.jpg',
+    title: 'Zéti',
+    description: 'Kaposvár',
+  },{
+    src: '/9.jpg',
+    title: '12',
+    description: 'Kaposvár',
+  },{
+    src: '/10.jpg',
+    title: '2',
+    description: 'Kaposvár',
+  },{
+    src: '/11.jpg',
+    title: 'Nike Mercurial',
+    description: 'Kaposvár',
+  },{
+    src: '/12.jpg',
+    title: 'Nike Phantom',
+    description: 'Kaposvár',
+  },{
+    src: '/13.jpg',
+    title: 'Cold Sunset',
+    description: 'Deseda',
+  },{
+    src: '/14.jpg',
+    title: 'Lendi',
+    description: 'Deseda',
+  },{
+    src: '/15.jpg',
+    title: 'Strandröpi',
+    description: 'Deseda',
+  },{
+    src: '/16.jpg',
+    title: '@Kbbenedek',
+    description: 'Pécs',
+  },{
+    src: '/17.jpg',
+    title: 'Cut',
+    description: 'Kaposvár',
+  },{
+    src: '/18.jpg',
+    title: 'Vrum Vrum',
+    description: 'Kaposmérő',
+  },{
+    src: '/19.jpg',
+    title: 'Nike',
+    description: 'Kaposvár',
+  },{
+    src: '/20.jpg',
+    title: 'Mirror selfie',
+    description: 'Kaposvár',
+  },{
+    src: '/21.jpg',
+    title: 'Nincs hozzászólás',
+    description: 'Zimány',
+  },{
+    src: '/22.jpg',
+    title: 'Ford Galaxy',
+    description: 'Zimány',
   },
 ]
